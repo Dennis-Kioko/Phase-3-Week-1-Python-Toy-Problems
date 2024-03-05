f035cf472b345ea039f0fbe8205a19a60cac64ab
denniskioko <kiokodennis6990@gmail.com>
2024-03-05 11:10:39 +0300

    challenge-2

--- Toy-Problems/code-challenge-2.py
+++ Toy-Problems/code-challenge-2.py
@@ -1,3 +1,22 @@
-# Number of boxes N. Numbered from (0 to N-1)
-# K-th box contains A[K] bricks
-# A = [N]
+def digit_sum(num):
+        return sum(int(digit) for digit in str(num))
+
+def solution(A):
+    max_sum = -1
+
+    # Iterate through all pairs of numbers in the array
+    for i in range(len(A)):
+        for j in range(i + 1, len(A)):
+            sum_i = digit_sum(A[i])
+            sum_j = digit_sum(A[j])
+
+            # Check if the sums of digits are equal
+            if sum_i == sum_j:
+                # Update the maximum sum if the current sum is greater
+                max_sum = max(max_sum, A[i] + A[j])
+
+    return max_sum
+
+print(solution([51, 71, 17, 42])) 
+print(solution([42, 33, 60]))       
+print(solution([51, 32, 43]))       
