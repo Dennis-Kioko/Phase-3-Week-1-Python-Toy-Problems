10e3f2dd3dba8bb9acac25a07ba3b0d7d05dcd62
denniskioko <kiokodennis6990@gmail.com>
2024-03-05 11:24:46 +0300

    challenge-3

---
+++ Toy-Problems/code-challenge-3.py
@@ -0,0 +1,16 @@
+def solution(N):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    result = ""
+
+    if N % 2 == 0 or N % 3 == 0:
+        for i in range(N):
+            result += alphabet[i % 26]
+    else:
+        for i in range(N):
+            result += alphabet[(i % 25) + 1]
+
+    return result
+
+print(solution(3))   
+print(solution(5))   
+print(solution(30))  
